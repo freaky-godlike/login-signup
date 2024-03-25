79c6cfa56c80832458554e652377701bd97b19cc
freaky-godlike <145795462+freaky-godlike@users.noreply.github.com>
2024-03-25 21:09:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import LoginSignup from './Components/LoginSignUp/LoginSignup';
+function App(){
+  return (
+    <div className="App">
+      <LoginSignup/>
+    </div>
+  );
+}
+export default App;
